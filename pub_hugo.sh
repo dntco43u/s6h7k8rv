bbfe1e20af8de4281830b1407449c48a3b96a00e
dntco43u <xqbty8po-dntco43u@yahoo.com>
2025-12-23 10:23:26 +0900

    update

---
+++ pub_hugo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# hugo clean -> build -> publsh
+
+source /home/dev/.bashrc
+source /home/dev/.local/bin/utils.sh
+
+container_name=hugo
+rm -rf /opt/$container_name/data/public/*
+cd /opt/$container_name && docker compose rm -f -s && docker compose up -d && docker exec -it $container_name date +"%Z"
+sudo tail -fn0 /var/lib/docker/containers/"$(docker inspect --format="{{.Id}}" $container_name)/local-logs/container.log" | \
+while read -r line; do
+  echo "$line"
+  if [[ "$line" == *"Web Server is available"* ]]; then
+    break
+  fi
+done
+cd /opt/$container_name/data/public || exit
+git add . && git commit -m "update" && git push -u origin main
