df6023b6cfd38a6cc7e1c3f3804652e69c723c93
dntco43u <xqbty8po-dntco43u@yahoo.com>
2025-12-30 09:47:26 +0900

    update

--- pub_hugo.sh
+++ pub_hugo.sh
@@ -5,6 +5,14 @@ source /home/dev/.bashrc
 source /home/dev/.local/bin/utils.sh
 
 container_name=hugo
+hugo_env=$(docker exec -it $container_name hugo version)
+hugo_version=$(echo "$hugo_env" | grep -o "hugo v[0-9.]*" | grep -o "[0-9.]*")
+if echo "$hugo_env" | grep -q "+extended"; then
+  hugo_extended=true
+else
+  hugo_extended=false
+fi
+
 rm -rf /opt/$container_name/data/public/*
 cd /opt/$container_name && docker compose rm -f -s && docker compose up -d && docker exec -it $container_name date +"%Z"
 sudo tail -fn0 /var/lib/docker/containers/"$(docker inspect --format="{{.Id}}" $container_name)/local-logs/container.log" | \
@@ -15,5 +23,10 @@ while read -r line; do
     break
   fi
 done
+
+echo "HUGO_VERSION=$hugo_version" > /opt/$container_name/data/public/.env
+echo "HUGO_EXTENDED=$hugo_extended" >> /opt/$container_name/data/public/.env
+cat /opt/$container_name/data/public/.env
 cd /opt/$container_name/data/public || exit
+#git rm -r --cached .
 git add . && git commit -m "update" && git push -u origin main
